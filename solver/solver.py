f60b0bb9bac76a592ffa3b2d8455dfa938c505de
dmac <DaraMac@users.noreply.github.com>
2021-10-04 21:06:42 +0100

    Restructure directory, finish first draft of DFS algorithm

--- solver/solver.py
+++ solver/solver.py
@@ -1,5 +1,5 @@
-# developed in Python 3.9.7
-# define sudoku as a 9x9 2D list with 0s for unfilled in values
+# developed in Python 3.9
+# define sudoku as a 9x9 2D list with 0s for unfilled in values, where each sublist represents a row, starting indexing from top left corner
 
 # nums = frozenset([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
@@ -65,3 +65,43 @@ def check_box(grid, x, y) -> bool:
         else:
             return False
     return True
+
+
+def solve(grid):
+    """Takes a valid sudoku grid and returns it solved."""
+    original_layout = [[True if grid[y][x] != 0 else False for x in range(9)] for y in range(9)]
+
+    i = 0
+    while i < 81:
+        print(i)
+        y = i // 9
+        x = i % 9
+
+        if original_layout[y][x]:
+            i += 1
+            continue
+
+        while grid[y][x] < 9:
+            grid[y][x] += 1
+            if check_location(grid, x, y):
+                i += 1
+                break
+        else:
+            grid[y][x] = 0
+            while not original_layout[i // 9][i % 9]:
+                i -= 1
+
+    return grid
+
+
+def read_grid(filename: str):
+    """Reads in a single sudoku puzzle in txt format and returns it as a 2D list.
+
+    Expects spaces between numbers in a row, no spaces between rows, and 0s represent unfilled values.
+    Doesn't make any assumptions or checks on dimensions."""
+
+    grid = []
+    with open(filename) as f:
+        for line in f:
+            grid.append(list(map(int, line.split())))
+    return grid

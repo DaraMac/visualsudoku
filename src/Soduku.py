b3b54421e32576190e7913b4ee9791745b6698eb
Zuoyun <ludwigjer@me.com>
2021-10-04 09:44:31 +0100

    name fix

--- src/Soduku.py
+++ src/Soduku.py
@@ -58,17 +58,17 @@ def reorder(points):
 def img_stack(img_array,scale):
     rows = len(img_array)
     cols = len(img_array[0])
-    rowsAvailable = isinstance(img_array[0], list)
+    rows_available = isinstance(img_array[0], list)
     width = img_array[0][0].shape[1]
     height = img_array[0][0].shape[0]
-    if rowsAvailable:
+    if rows_available:
         for x in range ( 0, rows):
             for y in range(0, cols):
                 img_array[x][y] = cv2.resize(img_array[x][y], (0, 0), None, scale, scale)
                 if len(img_array[x][y].shape) == 2: img_array[x][y]= cv2.cvtColor( img_array[x][y], cv2.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
+        img_pipeline = np.zeros((height, width, 3), np.uint8)
+        hor = [img_pipeline]*rows
+        hor_con = [img_pipeline]*rows
         for x in range(0, rows):
             hor[x] = np.hstack(img_array[x])
             hor_con[x] = np.concatenate(img_array[x])

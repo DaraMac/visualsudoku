ebb24b2713a0dd1ae48a73b741b10d7dfa900911
dmac <DaraMac@users.noreply.github.com>
2021-10-28 17:36:05 +0100

    Fix new lines and some naming conventions, add the start of input to test on

--- solver/ordersum.py
+++ solver/ordersum.py
@@ -1,40 +1,51 @@
-#!
+# Input to this as a function should be a list of dicts
+# where the keys in each dict are numbers 0-9 inclusive (0 meaning a blank cell)
+# and the value for each key is the log of the probability the digit recogniser gives for that
+# key being the digit in the cell
+#
+# The list will probably be length 81 corresponding directly to the whole Sudoku grid
+# and each dict could have the full 10 entries unless we decide to cut off ones with too low
+# or zero probability
+#
+# The configuration produced will then have to be checked to see if it's valid in its current state
+# and then checked to see if it has a (unique?) solution
+# This might happen here or in another function outside this
+
 import heapq
 
-input = [ [1, 2, 3], [0, 7, 8], [3], [2, 4] ]
+test_input = [[1, 2, 3], [0, 7, 8], [3], [2, 4]]
+dict_input = [{1:1, 2:2, 3:3}, {0:0, 7:7, 8:8}, {3:3}, {2:2, 4:4}
 
 # Make sure lists are sorted
-for l in input:
+for l in test_input:
 	l.sort(reverse=True)
 
 # Figure out biggest sum
-sum = sum([ l[0] for l in input])
+current_sum = sum([l[0] for l in test_input])
 
-# Make starting object
-object = (-sum , input)
+# Make starting item
+item = (-current_sum , test_input)
 
-heap = [ ] 
+heap = []
 seen = set()
-heapq.heappush(heap, object)
-seen.add(str(object))
+heapq.heappush(heap, item)
+seen.add(str(item))
 
 while len(heap) > 0:
-	object = heapq.heappop(heap)
-	seen.discard(str(object))
+	item = heapq.heappop(heap)
+	seen.discard(str(item))
 	assert len(heap) == len(seen)
-	sum = -object[0]
-	lol = object[1]
+	current_sum = -item[0]
+	lol = item[1]
 
-	print("{} = sum({})".format(-object[0], [l[0] for l in lol]))
+	print("{} = sum({})".format(-item[0], [l[0] for l in lol]))
 
 	for i in range(len(lol)):
 		if len(lol[i]) > 1:
-			newsum = sum - lol[i][0] + lol[i][1]
+			newsum = current_sum - lol[i][0] + lol[i][1]
 			newlist = lol.copy()
 			newlist[i] = lol[i][1:]
-			object = (-newsum, newlist)
-			if not (str(object) in seen):
-				heapq.heappush(heap, object)
-				seen.add(str(object))
-
-
+			item = (-newsum, newlist)
+			if not (str(item) in seen):
+				heapq.heappush(heap, item)
+				seen.add(str(item))

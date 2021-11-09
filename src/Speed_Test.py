fa74f525501499f1962aea216b65293250193238
dmac <DaraMac@users.noreply.github.com>
2021-11-09 17:07:08 +0000

    Get SA ready for useage in speed_tests, write main body of speed_test code

--- src/Speed_Test.py
+++ src/Speed_Test.py
@@ -1,4 +1,24 @@
+import csv
 import timeit
 import brute_solver
-import constraint_solver
-import SA
+import solver_utils as util
+# import constraint_solver
+# import SA
+
+def convert_sudoku(sudoku):
+    """Takes a sudoku as a single 81 digit string with 0's for blanks and returns 2D grid."""
+    sudoku_list = [list(map(int, sudoku[i*9:i*9+9])) for i in range(9)]
+    return sudoku_list
+
+with open("small_test_set.csv", newline='') as f:
+    reader = csv.reader(f)
+    tests = []
+    for line in reader:
+        tests.append(convert_sudoku(line[0]))
+
+# t = timeit.Timer("brute_solver.solve(tests[0])", globals=globals())
+
+times = {"brute":[], "constraint":[], "anneal":[]}
+# choosing 100 as that is the length of the small_test_set file
+for i in range(100):
+    times["brute"].append(timeit.timeit(f"brute_solver.solve(tests[{i}])", globals=globals(), number=1))

4e824a2b90f7fabbef8fd30d52435e65f4a4e801
dmac <DaraMac@users.noreply.github.com>
2021-11-16 19:58:40 +0000

    Start making different histograms on the test data, also add some code to use the larger data set if we want that

--- src/speed_test.py
+++ src/speed_test.py
@@ -21,28 +21,37 @@ with open("small_test_set.csv", newline='') as f:
     for line in reader:
         tests.append(convert_sudoku(line[0]))
 
-# t = timeit.Timer("brute_solver.solve(tests[0])", globals=globals())
+
+# with open("test_set.csv", newline='') as f:
+#     reader = csv.reader(f)
+#     tests = []
+#     for line in reader:
+#         tests.append(convert_sudoku(line[0]))
+
 
 times = {"brute":[], "constraint":[], "anneal":[]}
 # choosing 100 as that is the length of the small_test_set file
 for i in range(100):
+# for i in range(1000): # for the larger test set file
     times["brute"].append(timeit.timeit(f"brute_solver.solve(tests[{i}])", globals=globals(), number=1))
     times["constraint"].append(timeit.timeit(f"constraint_solver.solve(tests[{i}])", globals=globals(), number=1))
     times["anneal"].append(timeit.timeit(f"SA.solve(p)", setup=f"p = np.array(tests[{i}])", globals=globals(), number=1))
 
-print(f"Brute:\nMean {mean(times['brute'])}\nMax {max(times['brute'])}\nMin {min(times['brute'])}\n")
-print(f"Constraint:\nMean {mean(times['constraint'])}\nMax {max(times['constraint'])}\nMin {min(times['constraint'])}\n")
-print(f"Simulated Annealing:\nMean {mean(times['anneal'])}\nMax {max(times['anneal'])}\nMin {min(times['anneal'])}\n")
-
-x = np.linspace(0, 2, 100)
-
+# print(f"Brute:\nMean {mean(times['brute'])}\nMax {max(times['brute'])}\nMin {min(times['brute'])}\n")
+# print(f"Constraint:\nMean {mean(times['constraint'])}\nMax {max(times['constraint'])}\nMin {min(times['constraint'])}\n")
+# print(f"Simulated Annealing:\nMean {mean(times['anneal'])}\nMax {max(times['anneal'])}\nMin {min(times['anneal'])}\n")
+# 
 brute = times["brute"]
 constraint = times["constraint"]
 anneal = times["anneal"]
 
-interactive(True) # to make plots show while testing
+
+######################################################################
+# Graphs
+
+# interactive(True) # to make plots show while testing
 # Note that even in the OO-style, we use `.pyplot.figure` to create the figure.
-fig, ax = plt.subplots()  # Create a figure and an axes.
+# fig, ax = plt.subplots()  # Create a figure and an axes.
 # ax.plot(x, x, label='linear')  # Plot some data on the axes.
 # ax.plot(x, x**2, label='quadratic')  # Plot more data on the axes...
 # ax.plot(x, x**3, label='cubic')  # ... and some more.
@@ -50,3 +59,24 @@ fig, ax = plt.subplots()  # Create a figure and an axes.
 # ax.set_ylabel('y label')  # Add a y-label to the axes.
 # ax.set_title("Simple Plot")  # Add a title to the axes.
 # ax.legend()  # Add a legend.
+
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+
+fig.suptitle("Speed comparison of Sudoku solving algorithms")
+
+ax1.hist(brute)
+ax1.set_title("Brute Force")
+
+ax2.hist(constraint)
+ax2.set_title("Constraint Solver")
+
+ax3.hist(anneal)
+ax3.set_title("Simulated Annealing")
+
+fig2, ax = plt.subplots()
+ax.hist([brute, constraint, anneal],
+        bins=30,
+        label=["brute", "constraint", "simulated annealing"])
+ax.legend()
+
+plt.show()
